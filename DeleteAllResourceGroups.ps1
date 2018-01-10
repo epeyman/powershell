15c418aebc30bfe7476799b7f593b12baa4f1885
Peyman Nasirifard <penasiri@microsoft.com>
2018-01-10 16:28:33 +0100

    Deleting all Azure resrouce groups
    It deletes all resource groups except the ones which are mentioned on the list. USE WITH CAUTION!

---
+++ DeleteAllResourceGroups.ps1
@@ -0,0 +1,17 @@
+﻿# A simple script to delete all Azure resources recursively
+
+$AzureRmAccount = Login-AzureRmAccount -EnvironmentName AzureCloud
+
+# List of resource groups which should not be deleted. It is comma separated.
+
+$noDelRes = "Important1", "Important2"
+  
+$rgName = Get-AzureRmResourceGroup 
+
+Foreach($name in $rgName)
+{
+    Write-Host $name.ResourceGroupName
+    if ($noDelRes -notcontains $name.ResourceGroupName) {
+        Remove-AzureRmResourceGroup -Name $name.ResourceGroupName -Verbose -Force
+    }
+}
